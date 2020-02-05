d48bb7f322e1bc947264775d5171585a7b74f834
Christian Drappi <christiandrappi@gmail.com>
2020-02-05 14:53:01 -0800

    more

--- profile.sh
+++ profile.sh
@@ -1,7 +1,10 @@
 export PROJECT_ALIAS="caucus"
 
+source ${CAUCUS_PATH}/clients/python/venv/bin/activate;
+
 alias cdf="${PROJECT_ALIAS}; cd apps/web";
 alias cdb="${PROJECT_ALIAS}; cd services/api";
+alias cdp="${PROJECT_ALIAS}; cd clients/python";
 
 alias sf="cdf; npm run dev";
 alias sb="cdb; cargo run";
