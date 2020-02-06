5edb7425d25b10243797fac698a550212d24144a
Christian Drappi <christiandrappi@gmail.com>
2020-02-05 16:09:19 -0800

    ok

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-export PROJECT=caucus-project
-export PRODUCT=caucus-product
+export PROJECT=caucus
+export PRODUCT=caucus
 export KUBE_CONTEXT=caucus-context
 export KUBE_NAMESPACE=caucus-namespace
 
