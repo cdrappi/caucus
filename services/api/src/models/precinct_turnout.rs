c4a28322c3fa493c575e87c4a06064bb057451f4
Christian Drappi <christiandrappi@gmail.com>
2020-02-07 18:53:20 -0800

    frontend voting

--- services/api/src/models/precinct_turnout.rs
+++ services/api/src/models/precinct_turnout.rs
@@ -34,17 +34,10 @@ impl PrecinctTurnout {
         precinct: &String,
         turnout: i32,
     ) -> Result<PrecinctTurnout, Error> {
-        let new_edit_trail = (
-            precinct_turnout_edit_trails::dsl::user_id.eq(user_id),
-            precinct_turnout_edit_trails::dsl::org.eq(org),
-            precinct_turnout_edit_trails::dsl::precinct.eq(precinct),
-            precinct_turnout_edit_trails::dsl::turnout.eq(turnout),
-        );
-        let edit_trail =
-            diesel::insert_into(precinct_turnout_edit_trails::table)
-                .values(&new_edit_trail)
-                .get_result::<PrecinctTurnoutEditTrail>(conn)
-                .expect("Unable to append precinct turnout edit trail");
+        let edit_trail = PrecinctTurnout::create_edit_trail(
+            conn, user_id, org, precinct, turnout,
+        )
+        .expect("Unable to append precinct turnout edit trail");
 
         let updated_precinct_turnout = (
             precinct_turnouts::dsl::edit_trail_id.eq(edit_trail.id),
@@ -58,5 +51,23 @@ impl PrecinctTurnout {
             .do_update()
             .set(updated_precinct_turnout)
             .get_result::<PrecinctTurnout>(conn);
+    }
+
+    fn create_edit_trail(
+        conn: &PgConnection,
+        user_id: i32,
+        org: &String,
+        precinct: &String,
+        turnout: i32,
+    ) -> Result<PrecinctTurnoutEditTrail, Error> {
+        let new_edit_trail = (
+            precinct_turnout_edit_trails::dsl::user_id.eq(user_id),
+            precinct_turnout_edit_trails::dsl::org.eq(org),
+            precinct_turnout_edit_trails::dsl::precinct.eq(precinct),
+            precinct_turnout_edit_trails::dsl::turnout.eq(turnout),
+        );
+        diesel::insert_into(precinct_turnout_edit_trails::table)
+            .values(&new_edit_trail)
+            .get_result::<PrecinctTurnoutEditTrail>(conn)
     }
 }
